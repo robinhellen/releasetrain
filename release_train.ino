3d9882ffdd97dcf1c2718bbe9cd2554aeed96509
Robin Hellen <robin.hellen@red-gate.com>
2016-03-04 10:47:25 +0000

    Initial version.

---
+++ release_train.ino
@@ -0,0 +1,239 @@
+struct InputState {
+  boolean switchState;
+  boolean section1;
+  boolean section2;
+  boolean section3;
+
+  void print();
+};
+
+class State {
+  public:
+    virtual State* getNextState(InputState input) = 0;
+    virtual void enter() = 0;
+    virtual void printName() = 0;
+};
+
+class AtRest : public State {
+  public:
+    virtual State* getNextState(InputState input);
+    virtual void enter();
+    virtual void printName();
+};
+
+class GoingForwards : public State {
+  public:
+    virtual State* getNextState(InputState input);
+    virtual void enter();
+    virtual void printName();  
+};
+
+class GoingBackwards : public State {
+  public:
+    virtual State* getNextState(InputState input);
+    virtual void enter();
+    virtual void printName();    
+};
+
+State* AtRest::getNextState(InputState input)
+{
+  if(input.switchState)
+    return new GoingForwards();
+  return NULL;
+}
+
+State* GoingForwards::getNextState(InputState input)
+{
+  if(input.section3)
+    return new GoingBackwards();
+  return NULL;
+}
+
+State* GoingBackwards::getNextState(InputState input)
+{
+  if(input.section1)
+    return new AtRest();
+  return NULL;
+}
+
+void AtRest::enter() {
+  digitalWrite(8, LOW);
+  digitalWrite(11, LOW);  
+}
+
+void GoingForwards::enter() {
+  digitalWrite(8, LOW);
+  digitalWrite(11, HIGH);  
+}
+
+void GoingBackwards::enter() {
+  digitalWrite(8, HIGH);
+  digitalWrite(11, LOW);  
+}
+
+void AtRest::printName() {
+  Serial.print("Stopped");
+}
+void GoingForwards::printName() {
+  Serial.print("Forward");
+}
+void GoingBackwards::printName() {
+  Serial.print("Backward");
+}
+
+void InputState::print()
+{
+  Serial.print("Button: ");
+  Serial.print(this->switchState ? "on " : "off");
+  Serial.print(" Section 1: ");
+  Serial.print(this->section1 ? "on " : "off");
+  Serial.print(" Section 2: ");
+  Serial.print(this->section2 ? "on " : "off");
+  Serial.print(" Section 3: ");
+  Serial.print(this->section3 ? "on " : "off");
+}
+
+State *currentState;
+
+void setup() {  
+  pinMode(3, INPUT);
+  pinMode(4, INPUT);
+  pinMode(5, INPUT);
+  pinMode(6, INPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(11, OUTPUT);
+  // put your setup code here, to run once:
+
+  setPwmFrequency(9, 256);
+  Serial.begin(9600);
+  Serial.println("Starting");
+  currentState = new AtRest();
+  currentState->enter();
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int speedo = doPwm(9, A1, A2, A3);
+
+  InputState state = readInputs();
+
+  Serial.print("Input state is: ");
+  state.print();
+  Serial.print("Currently in ");
+  currentState->printName();
+  Serial.println(" state");
+
+  State * newState = currentState->getNextState(state);
+  if(newState == NULL) return;
+
+  newState->enter();
+  delete currentState;
+  currentState = newState;  
+}
+
+struct InputState readInputs()
+{
+  bool sect1 = digitalRead(5) == LOW;
+  bool sect2 = digitalRead(6) == LOW;
+  bool sect3 = digitalRead(4) == LOW;
+  bool button = digitalRead(3) == HIGH;
+  
+  InputState state = {button, sect1, sect2, sect3};
+
+  return state;
+}
+
+// 1 second's worth of PWM
+int doPwm(int pin, int speedoPin, int speedPin, int trimmerPin)
+{
+  int speedoAccum = 0;
+  for(int i = 0; i < 100; i++)
+  {
+    int val = analogRead(speedPin);
+    int rangeBottom = analogRead(trimmerPin);
+    int bottom = map(rangeBottom, 0, 1023, 0, 255);
+    int speed = map(val, 0, 1023, bottom, 225);
+    if(speed == bottom) speed = 0;
+    speedoAccum += doPwmSingle(pin, speed, speedoPin);
+  }
+  return speedoAccum / 100;
+}
+
+// Single PWM cycle at 100Hz
+int doPwmSingle(int pin, byte speed, int speedoPin)
+{
+  /*digitalWrite(pin, HIGH);
+  int onPeriod = 10000 * speed / 256;
+  delayMicroseconds(onPeriod);
+  digitalWrite(pin, LOW);
+  delayMicroseconds(10000 - onPeriod);*/
+  analogWrite(pin, speed);
+
+  return analogRead(speedoPin);
+}
+
+/**
+ * Divides a given PWM pin frequency by a divisor.
+ *
+ * The resulting frequency is equal to the base frequency divided by
+ * the given divisor:
+ *   - Base frequencies:
+ *      o The base frequency for pins 3, 9, 10, and 11 is 31250 Hz.
+ *      o The base frequency for pins 5 and 6 is 62500 Hz.
+ *   - Divisors:
+ *      o The divisors available on pins 5, 6, 9 and 10 are: 1, 8, 64,
+ *        256, and 1024.
+ *      o The divisors available on pins 3 and 11 are: 1, 8, 32, 64,
+ *        128, 256, and 1024.
+ *
+ * PWM frequencies are tied together in pairs of pins. If one in a
+ * pair is changed, the other is also changed to match:
+ *   - Pins 5 and 6 are paired on timer0
+ *   - Pins 9 and 10 are paired on timer1
+ *   - Pins 3 and 11 are paired on timer2
+ *
+ * Note that this function will have side effects on anything else
+ * that uses timers:
+ *   - Changes on pins 3, 5, 6, or 11 may cause the delay() and
+ *     millis() functions to stop working. Other timing-related
+ *     functions may also be affected.
+ *   - Changes on pins 9 or 10 will cause the Servo library to function
+ *     incorrectly.
+ *
+ * Thanks to macegr of the Arduino forums for his documentation of the
+ * PWM frequency divisors. His post can be viewed at:
+ *   http://www.arduino.cc/cgi-bin/yabb2/YaBB.pl?num=1235060559/0#4
+ */
+void setPwmFrequency(int pin, int divisor) {
+  byte mode;
+  if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 64: mode = 0x03; break;
+      case 256: mode = 0x04; break;
+      case 1024: mode = 0x05; break;
+      default: return;
+    }
+    if(pin == 5 || pin == 6) {
+      TCCR0B = TCCR0B & 0b11111000 | mode;
+    } else {
+      TCCR1B = TCCR1B & 0b11111000 | mode;
+    }
+  } else if(pin == 3 || pin == 11) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 32: mode = 0x03; break;
+      case 64: mode = 0x04; break;
+      case 128: mode = 0x05; break;
+      case 256: mode = 0x06; break;
+      case 1024: mode = 0x7; break;
+      default: return;
+    }
+    TCCR2B = TCCR2B & 0b11111000 | mode;
+  }
+}
+
